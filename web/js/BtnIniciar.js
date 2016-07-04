3cd744374030b7bb631f5e8982c54e836dbaa18b
Usuario <Usuario@A5-M07>
2016-07-04 17:41:28 -0300

    Hola

---
+++ web/js/BtnIniciar.js
@@ -0,0 +1,15 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var boton=document.querySelector("#btnIniciar");
+//boton.setAttribute("onclick","Iniciar()");
+//boton.addEventListener("click",Iniciar);
+
+
+function Iniciar(){
+    alert("Hola Mundo");
+}
+
+
